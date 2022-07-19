7417bde3e13137207eab8a7c000a8c33c54ef2b6
Lukáš Daněk <dr.danek@gmail.com>
2022-07-19 21:30:46 +0200

    use of static

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
-import express from 'express'
-import url from 'url'
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
 import fs from 'fs'
 
-const app = express()
+dotenv.config();
+
+const app: Express = express()
 const protocol = process.env.PROTOCOL || 'http'
 const port = process.env.PORT || 8080
 const host = process.env.HOST || '0.0.0.0'
@@ -14,29 +16,9 @@ app.set('credentials', app.get('protocol')==='https' ? {
 app.set('port', port);
 app.set('host', host);
 
+app.use('/', express.static('www'));
 app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-const publicDir = __dirname+'/../www'
-app.use(express.static(publicDir))
-app.get('*', async (req, res) => {
-  const  parsedUrl = url.parse(req.url, true)
-  if(parsedUrl === null) {
-    res.sendFile(publicDir+'index.html')
-    return
-  } 
-  const file = __dirname+parsedUrl!.path.replace(/^\/+|\/+$/g,"")
-  if(file === '' || (()=>{fs.access(file, fs.constants.F_OK, (err) => {
-    if (err) {
-      console.error(err)
-      return true
-    }
-  })
-  return true
-  })())
-    res.sendFile(file)
-})
-
-app.post('/calc', (req, res) => {
+app.post('/calc', (req: Request, res: Response) => {
   const LoanCalc = require('loan-calc')
   try{
     const result:number = LoanCalc.paymentCalc({
